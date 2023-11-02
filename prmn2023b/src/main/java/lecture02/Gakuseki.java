8152147040f7efe2fb660244284586f11a36688a
katatsumuri <maimaidance0620@gmail.com>
2023-11-02 18:19:04 +0900

    lecture02練習問題3の追加

--- prmn2023b/src/main/java/lecture02/Gakuseki.java
+++ prmn2023b/src/main/java/lecture02/Gakuseki.java
@@ -1,5 +1,6 @@
 package lecture02;
 
+import java.util.Objects;
 import java.util.Scanner;
 
 public class Gakuseki {
@@ -12,5 +13,26 @@ public class Gakuseki {
         }else{
             System.out.println("error!!");
         }
+    }
+
+    void pass(String[][] number){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("学籍番号を入力してください");
+        String gakuseki = scan.next();
+        for (String[] strings : number) {
+            if (Objects.equals(gakuseki, strings[0])) {
+                System.out.println("パスワードを入力してください");
+                String password = scan.next();
+                if (Objects.equals(password, strings[1])) {
+                    System.out.println("ログイン完了");
+                    return;
+                } else {
+                    System.out.println("不正なアクセス");
+                    System.exit(0);
+                }
+            }
+        }
+        System.out.println("error!!");
+        return;
     }
 }
---
+++ prmn2023b/src/main/java/lecture02/prac3.java
@@ -0,0 +1,10 @@
+package lecture02;
+
+public class prac3 {
+    public static void main(String[] args) {
+        String[][] number = {{"B2220010","a"},{"B2220020","b"},{"B2220030","c"},{"B2220040","d"},{"b2220050","e"}};
+        Gakuseki gakuseki = new Gakuseki();
+        gakuseki.pass(number);
+        return;
+    }
+}

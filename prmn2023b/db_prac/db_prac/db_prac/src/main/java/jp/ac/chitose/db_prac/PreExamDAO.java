d7096d50bf42a64ede8e16f9f26cbe65c15a6d2b
katatsumuri <maimaidance0620@gmail.com>
2023-11-17 23:36:15 +0900

    PreExamクラスとPreExamDAO区r巣クラスのの実装è

---
+++ prmn2023b/db_prac/db_prac/db_prac/src/main/java/jp/ac/chitose/db_prac/PreExamDAO.java
@@ -0,0 +1,32 @@
+package jp.ac.chitose.db_prac;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class PreExamDAO {
+    private static final String URL = "~/h2db/db_prac;Mode=PostgreSQL;AUTO_SERVER=TRUE;";
+    private static final String USER_NAME = "b2222520";
+    public static final String USER_PASS = "Yosh!8740620";
+
+    public List<PreExam> selectpreExams(int lessThan) throws SQLException {
+        List<PreExam> returning = new ArrayList<>();
+        String SQL = "select * from 学生情報 where ? < 得点 ";
+
+        try (Connection conn = DriverManager.getConnection(URL,USER_NAME,USER_PASS);PreparedStatement stmt = conn.prepareStatement(SQL)){
+            stmt.setInt(1,lessThan);
+            ResultSet result = stmt.executeQuery();
+            while (result.next()){
+                String  col1 = result.getString("学生コード");
+                String  col2 = "null";
+                String col3 = "null";
+                int cor4 = result.getInt("得点");
+                PreExam preExam = new PreExam(col1, col2, col3, cor4);
+                returning.add(preExam);
+            }
+            return returning;
+        }
+
+    }
+
+}
